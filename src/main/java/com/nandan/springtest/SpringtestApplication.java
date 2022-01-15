699e048f53a7d8cc8fb108c0961f6c9731c9ce90
Nandan <nandan.gupta@gmail.com>
2022-01-14 17:58:35 -0800

    first commit

---
+++ src/main/java/com/nandan/springtest/SpringtestApplication.java
@@ -0,0 +1,21 @@
+package com.nandan.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringtestApplication {
+
+public static void main(String[] args) {
+SpringApplication.run(SpringtestApplication.class, args);
+}
+
+@GetMapping("/hello")
+public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+return String.format("Hello %s!", name);
+}
+}
